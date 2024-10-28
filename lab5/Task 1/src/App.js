c784811aa787bf33f3a7c7b74d1a054534ba5918
NullL1f3 <NullL1f3@gmail.com>
2024-10-28 09:52:32 +0300

    Add files via upload

---
+++ lab5/Task 1/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	let age = 12;
+	let name = 'James';
+	return (
+		<span>
+			{name} is <strong>{age}</strong> years old
+		</span>
+	);
+}
+
+export default App;
---
+++ lab5/Task 2/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+	const customer = {
+		first_name: 'Bob',
+		last_name: 'Dylan',
+	};
+	return (
+		<div>
+			<h1>My name is {customer.first_name}</h1>
+			<h2>My last name is {customer.last_name}</h2>
+		</div>
+	);
+}
+
+export default App;
